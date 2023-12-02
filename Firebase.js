530509d5dfa7032fa9c35caa23594e6f23582db1
Bijosh Shagar <93047483+Itsbijoshuman@users.noreply.github.com>
2023-12-02 21:20:08 +0530

    Update Firebase.js

--- Firebase.js
+++ Firebase.js
@@ -5,13 +5,7 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
 
-  apiKey: "AIzaSyD_9ZU7ryEiY8d2bLLIPSzkw7Ij3O4s9uk",
-  authDomain: "petpals-mobile-29a39.firebaseapp.com",
-  projectId: "petpals-mobile-29a39",
-  storageBucket: "petpals-mobile-29a39.appspot.com",
-  messagingSenderId: "52222974480",
-  appId: "1:52222974480:web:ba0d240535055457664895",
-  measurementId: "G-763FBQHW88"
+
 
 };
 
